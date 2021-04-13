923acd250b9e457de133421cd64b8fda987612bc
Ryan Murphy <rymurphy12@gmail.com>
2021-04-13 15:26:53 -0400

    Added the Item model and EntityFramework Support

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace WishList.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+    }
+}
